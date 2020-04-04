d0d6e8693ccf438f52af42c85b5a05706d77039d
cheang-haoyuan <cheanghaoyuan@gmail.com>
2020-04-04 17:50:37 +1100

    file sort

---
+++ app.js
@@ -0,0 +1,47 @@
+// reused code from a previous group project
+// Git repo: YomigaeSu/info-30005-2019-pi
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+
+var app = express();
+
+// Initialise middlewares
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+// DB SETUP
+
+require('./src/models/db.js');
+
+// Serve static files under root path
+
+app.use('/', express.static('public'));
+
+// Capture API calls
+
+const routes = require('./src/routes/routes.js');
+app.use('/api', routes)
+
+// Capture invalid entrypoints
+
+app.all('*', (req, res) => {
+	//	res.sendStatus(403);
+
+	res.redirect('/err.html');
+
+})
+
+// START
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log('Express is up and runnin');
+})
