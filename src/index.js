c832001516a9831f4578981a39e8b7680a01b4d2
cheang-haoyuan <cheanghaoyuan@gmail.com>
2020-04-05 14:59:37 +1000

    added responsiveness

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Nav from "./components/nav.js";
-
-
-ReactDOM.render(<Nav />, document.getElementById('body'))
