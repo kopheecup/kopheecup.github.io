80e664efc4a79dae69beb926dc628fe13296d2d9
cheang-haoyuan <cheanghaoyuan@gmail.com>
2020-04-06 11:35:05 +1000

    finish about page and linked with home page

--- src/js/lib/ui.js
+++ src/js/lib/ui.js
@@ -3,6 +3,7 @@
 var ui = bindUIs();
 
   // actions
+ui.homeBtn.addEventListener('click', goHome);
 ui.introBtn.addEventListener('click', toIntro);
 ui.menuBtn.addEventListener('click', toggleMenu);
 
@@ -12,7 +13,10 @@ function bindUIs() {
   var ui = {};
 
   ui.content = document.getElementsByClassName("content");
+  ui.homeBtn = document.getElementById("home-btn")
   ui.introBtn = document.getElementById("about-btn-nav");
+  ui.galleryBtn = document.getElementById("gallery-btn-nav");
+  ui.contactBtn = document.getElementById("contact-btn-nav");
 
   ui.navLinks = document.getElementById("nav-link");
 
@@ -24,9 +28,30 @@ function bindUIs() {
   //functions
 
 function toIntro() {
+  if (ui.introBtn.classList.contains("active")) {
+    return;
+  }
+  else if (ui.galleryBtn.classList.contains("active")) {
+    window.location.href = "./intro.html";
+  }
+  else if (ui.contactBtn.classList.contains("active")) {
+    window.location.href = "./intro.html";
+  } else {
     window.location.href = "./public/intro.html";
+  }
 
 }
+
+
+function goHome() {
+  if (ui.homeBtn.classList.contains("home")) {
+    return;
+  } else {
+    window.location.href = "../index.html";
+  }
+
+}
+
 
 function toggleMenu() {
   if (ui.navLinks.classList.contains("show")) {
